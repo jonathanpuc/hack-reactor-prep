c4eefd3623007dfead8a74e201baa9f2c2c15b4c
Jonathan Puc <jonpucxyz@Jonathans-MacBook-Pro.local>
2017-11-14 20:27:53 +1100

    add solutions

---
+++ Module 1/108_getSmallestElementAtProperty.js
@@ -0,0 +1,24 @@
+/*
+Write a function called "getSmallestElementAtProperty".
+
+Given an object and a key, "getSmallestElementAtProperty" returns the smallest element in the array located at the given key. 
+
+Notes:
+* If the array is empty, it should return undefined.
+* If the property at the given key is not an array, it should return undefined. 
+* If there is no property at the key, it should return undefined.
+
+var obj = {
+  key: [2, 1, 5]
+};
+var output = getSmallestElementAtProperty(obj, 'key');
+console.log(output); // --> 1
+*/
+
+function getSmallestElementAtProperty(obj, key) {
+  let smallestEl;
+  if (obj[key] && Array.isArray(obj[key]) && obj[key].length > 0) {
+    smallestEl = obj[key].reduce((a, b) => Math.min(a, b));
+  }
+  return smallestEl;
+}
